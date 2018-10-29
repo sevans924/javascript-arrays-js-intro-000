31e06b5cdf0f67ce6594c9a29be4716c24bdf809
Sarah Elzinga <sevans924@gmail.com>
2018-10-29 14:55:19 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,8 +1,8 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
-function addElementToBeginningOfArray([1], 'a') {
-  var myArray = ['b', 'c', 'd']
-  myArray = ['a', ...]
+function addElementToBeginningOfArray([1], 'foo') {
+  const myArray = [1]
+  myArray = ['foo', ...]
 };
 
 function destructivelyAddElementToBeginningOfArray([1], 'a') {
