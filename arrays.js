d063bac8492e393e8b784c2c0873f6867a210e8e
Sarah Elzinga <sevans924@gmail.com>
2018-10-29 15:24:45 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,6 +5,6 @@ function addElementToBeginningOfArray(array, element) {
 };
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  array.push(element)
+  array.unshift(element)
   return array
 };
